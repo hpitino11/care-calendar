8e7b4e809e4afde98dbabc7e584d516bd3616e6a
hpitino11 <hannapitino11@gmail.com>
2026-05-14 15:41:33 -0400

    Update api url to railway

--- client/src/api.js
+++ client/src/api.js
@@ -1,3 +1,3 @@
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = 'https://railway.com/project/7ed336d0-f897-467b-bc21-ca4ee5d76b5f/service/c0602c14-e291-4a48-b961-b79e5039737d/settings?environmentId=ffe773bc-5bd5-4642-9635-d866ac28ebdf';
 
 export default BASE_URL;
