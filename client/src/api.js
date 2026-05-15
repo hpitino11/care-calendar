b2efde103ed90818642a2d4cc12322c4022d4841
hpitino11 <hannapitino11@gmail.com>
2026-05-15 04:41:25 -0400

    React sample delete

--- client/src/api.js
+++ client/src/api.js
@@ -1,3 +1,3 @@
-const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080';
+const BASE_URL = (process.env.REACT_APP_API_URL || 'http://localhost:8080').replace(/\/$/, '');
 
 export default BASE_URL;
